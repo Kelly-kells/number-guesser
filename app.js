ecb22e64762e95e1eb70b92f015cb3bc6c739b41
kelly-kells <kellskelechi@gmail.com>
2021-07-23 12:21:44 +0100

    validation

---
+++ app.js
@@ -0,0 +1,41 @@
+/*GAME FUNCTIONS
+- player must guess a number between
+  min-max
+- player gets certain amount of guesses
+- Notify player of guesses remaining
+- Notify player of answer if loose
+- Let player choose to play again
+*/
+
+//Game values
+let min = 1,
+  max = 10,
+  winningNum = 2,
+guessesLeft = 3;
+
+//UI elements
+const game = document.querySelector("#game"),
+  minNum = document.querySelector(".min-num"),
+  maxNum = document.querySelector(".max-num"),
+  guessBtn = document.querySelector("#guess-btn"),
+  guessInput = document.querySelector("#guess-input"),
+  message = document.querySelector(".message");
+
+//Assign UI min and max
+minNum.textContent = min;
+maxNum.textContent = max;
+
+//Listen for guesses
+guessBtn.addEventListener('click', function(){
+    let guess = parseInt(guessInput.value);
+    
+    //validate
+    if (isNaN(guess) || guess < min || guess > max ) {
+        setMessage(`Please enter a number ${min} and ${max}`, 'red');
+    }
+});
+    function setMessage(msg, color) {
+        message.style.color = color;
+        message.textContent = msg;
+        
+    }
